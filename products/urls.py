9a80c7dad5916b852a53eb868eb74d365d2b29bb
marsel01kg <mbekbolotov945@gmail.com>
2024-08-01 20:49:20 +0600

    hw6

--- products/urls.py
+++ products/urls.py
@@ -2,14 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/v1/categories/', views.category_list_api_view),
-    path('api/v1/categories/<int:id>/', views.category_detail_api_view),
+    path('api/v1/categories/', views.CategoryListAPIView.as_view()),
+    path('api/v1/categories/<int:id>/', views.CategoryDetailAPIView.as_view()),
 
-    path('api/v1/product/', views.products_list_api_view),
-    # path('api/v1/product/<int:id>/', views.products_detail_api_view),
+    path('api/v1/product/', views.ProductListAPIView.as_view()),
+    path('api/v1/product/<int:id>/', views.ProductDetailAPIView.as_view()),
 
-    path('api/v1/products/reviews/', views.rev_product_list_api_view),
-
-    path('api/v1/review/', views.review_list_api_view),
-    path('api/v1/review/<int:id>/', views.review_detail_api_view),
+    path('api/v1/review/', views.ReviewListAPIView.as_view()),
+    path('api/v1/review/<int:id>/', views.ReviewDetailAPIView.as_view()),
 ]
