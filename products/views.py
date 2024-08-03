0e9a216c6a6de618d077c93bbfec0575e03a1427
marsel01kg <mbekbolotov945@gmail.com>
2024-08-03 20:48:42 +0600

    hw4

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+
 
 from .models import Category, Product, Review
 from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer, ReviewProductSerializer, \
